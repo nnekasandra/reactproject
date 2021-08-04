f6090dafe093cfee7ff0a249d3361424a78eda7c
nnekasandra <nnekasandra@gmail.com>
2021-08-04 09:08:06 -0700

    made images responsive

--- src/image.js
+++ src/image.js
@@ -4,12 +4,12 @@ class Image extends React.Component{
     render(){
         return(
             <div className='container'>
-                <img src ='images/photo1.png' alt="flower plant"/>
-                <img src ='images/photo2.png' alt ="sofa" className ="photo2"/>
-                <img src ='images/photo3.png' alt ="bath tube" className ="photo3"/>
-                <img src ='images/photo4.png' alt ='lamp stand'/>
-                <img src ='images/photo5.png' alt="plant" className ="photo5"/>
-                <img src ='images/photo6.png' alt="basket" className ="photo5"/>
+                <img src ='images/photo1.png' alt="flower plant" className ="photo_1"/>
+                <img src ='images/photo2.png' alt ="sofa" className ="photo_2"/>
+                <img src ='images/photo3.png' alt ="bath tube" className ="photo_3"/>
+                <img src ='images/photo4.png' alt ='lamp stand' className ="photo_4"/>
+                <img src ='images/photo5.png' alt="plant" className ="photo_5"/>
+                <img src ='images/photo6.png' alt="basket" className ="photo_6"/>
             </div>
         );
     }
